7e04f63afdd73cd014ceb5c38478f5d2417a9023
mahesa <Mahesaarga07@gmail.com>
2026-05-09 19:27:39 +0700

    Menu Selection

--- CatalogLibrary.go
+++ CatalogLibrary.go
@@ -20,6 +20,26 @@ var BookData[nmax] Book
 func main(){
 	var menu int
 
+	fmt.Println("Menu")
+	fmt.Println("1. Add Data")
+	fmt.Println("2. Data Stored")
+	fmt.Println("3. Update Existed Data")
+	fmt.Println("4. Delete Existed Data")
+
+
+	fmt.Print("Entered the desired number :")
+	fmt.Scan(&menu)
+
+	if menu == 1 {
+		fmt.Print(create())
+	}else if menu == 2 {
+		fmt.Print(read())
+	}else if menu == 3 (
+		fmt.Print(update())
+	)else{
+		fmt.Print(delete())
+	}
+
 
 }
 
@@ -36,5 +56,5 @@ func update(){
 }
 
 func delete(){
-	
+
 }
