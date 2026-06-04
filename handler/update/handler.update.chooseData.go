c447db0139ff064e7f5d9f1d292fa79b2a836cee
naaupl <naupl.ramadhan@gmail.com>
2026-06-04 16:13:12 +0700

    created handler for update

---
+++ handler/update/handler.update.chooseData.go
@@ -0,0 +1,11 @@
+package handler
+
+import (
+	"fmt"
+)
+
+func PrintChooseDataToUpdate() {
+	fmt.Println("======================================================")
+	fmt.Println("        CHOOSE THE BOOK ID THAT WILL BE UPDATED")
+	fmt.Println("======================================================")
+}
