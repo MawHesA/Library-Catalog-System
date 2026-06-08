e746b8a235443dada6c87836c6345ecb75bced7a
naaupl <naupl.ramadhan@gmail.com>
2026-06-08 13:13:21 +0700

    created handler for create

---
+++ handler/create/handler.create.addedData.go
@@ -0,0 +1,11 @@
+package handler
+
+import (
+	"fmt"
+)
+
+func PrintSuccessAddedData() {
+	fmt.Println("======================================================")
+	fmt.Println("     Book Data Successfully Added And Sorted")
+	fmt.Println("======================================================")
+}
